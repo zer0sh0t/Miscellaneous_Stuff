0138cac14d96899e3caf9cceeb47033985f9372d
kumar-harin <haryn.k10@gmail.com>
2022-02-11 23:21:30 +0530

    ..

--- all_things_c/utils.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "utils.h"
-
-double add_from_utils(int a, int b)
-{
-    int c = a + b;
-    double d = (double)c;
-    return d;
-}
--- all_things_c/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-double add_from_utils(int a, int b);
-
-#endif
